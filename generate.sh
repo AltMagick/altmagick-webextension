d7a3f70521221949d43019b5922204224689ca59
Gaëtan L. H.-F. <81755261+gaetanlhf@users.noreply.github.com>
2024-05-11 16:13:08 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -2,6 +2,14 @@
 
 commit_hash=$(git rev-parse --short HEAD)
 
+if [ -f altmagick_chrome_*.zip ]; then
+    rm altmagick_chrome_*.zip
+fi
+
+if [ -f altmagick_firefox_*.zip ]; then
+    rm altmagick_firefox_*.zip
+fi
+
 cp manifest-chrome.json manifest.json
 zip -r altmagick_chrome_$commit_hash.zip css/ img/ js/ copy.html dashboard.html favicon.ico manifest.json popup.html
 rm manifest.json
